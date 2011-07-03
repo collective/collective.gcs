7615531ebae2fdbb09da7437a6cf92ef4506b414
Johannes Raggam <raggam-nl@adm.at>
2011-07-03 21:31:11 +0200

    initial stub

---
+++ README.txt
@@ -0,0 +1,6 @@
+Introduction
+============
+
+This product integrates a Google Custom Search for Plone
+
+This product may contain traces of nuts.
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.gcs',
+      version=version,
+      description="Google Custom Search for Plone",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone google search',
+      author='Johannes Raggam',
+      author_email='raggam-nl@adm.at',
+      url='https://github.com/collective/collective.gcs',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
