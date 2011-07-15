3893cf537a32d447f6c7a179745e87883903ab35
Johannes Raggam <raggam-nl@adm.at>
2011-07-15 16:43:19 +0200

    file reference fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0'
 setup(name='collective.gcs',
       version=version,
       description="Google Custom Search for Plone",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
